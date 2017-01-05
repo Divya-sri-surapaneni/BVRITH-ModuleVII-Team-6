770dc94c1b3de5de5043aa5a7f2140ae6d77ae13
Divya-sri-surapaneni <divyateddy03@gmail.com>
2017-01-05 10:48:11 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,29 +1,6 @@
-import unittest
+def multiply(a, b):
+return a * b;
 
-from pyramid import testing
-
-
-class ViewTests(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp()
-
-    def tearDown(self):
-        testing.tearDown()
-
-    def test_my_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'scaffolds')
-
-
-class FunctionalTests(unittest.TestCase):
-    def setUp(self):
-        from scaffolds import main
-        app = main({})
-        from webtest import TestApp
-        self.testapp = TestApp(app)
-
-    def test_root(self):
-        res = self.testapp.get('/', status=200)
-        self.assertTrue(b'Pyramid' in res.body)
+def test_numbers_3_4():
+assert multiply(4, 5) == 20
+assert multiply(4, 7) == 28
